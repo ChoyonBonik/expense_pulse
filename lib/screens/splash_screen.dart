c96c742b569bb828949c65a740173cc63fc17689
choyon <choyonbonik@gmail.com>
2026-08-18 16:45:33 +0600

    splash screen modified

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     // Show splash for 2 seconds then navigate; routing logic will redirect if unauthenticated
-    Timer(const Duration(seconds: 2), () {
+    Timer(const Duration(seconds: 10), () {
         if (!mounted) return;
         context.go('/');
       });
